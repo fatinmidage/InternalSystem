3b34d9eeb613ed43eb8ebb27c11c8e6fc68060f1
fatinmidage <fg1048596@live.com>
2018-10-13 17:35:16 +0800

    完成模型

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Item, Items_in, Items_out, Items_Total
 
 # Register your models here.
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id',  'name', 'item_model','unit', )
+
+
+@admin.register(Items_in)
+class Items_inAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item', 'quantity', 'price',
+                    'in_date', 'operator', 'location', 'is_deleted',)
+
+
+@admin.register(Items_out)
+class Items_outAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item', 'quantity', 'out_date',
+                    'operator', 'location', 'is_deleted',)
+
+
+@admin.register(Items_Total)
+class Items_TotalAdmin(admin.ModelAdmin):
+    list_display = ('item', 'quantity', 'is_deleted')
--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    item_model = models.CharField(max_length=20)
+    unit = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name+' '+self.item_model+' '+self.unit
+
+
+class Items_in(models.Model):
+    item = models.ForeignKey(Item,on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    total_price = models.FloatField()
+    in_date = models.DateField()
+    location = models.CharField(max_length=10)
+    operator = models.CharField(max_length=10)
+    is_deleted = models.BooleanField(default=False)
+
+
+
+class Items_out(models.Model):
+    item = models.ForeignKey(Item,on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField()
+    out_date = models.DateField()
+    location = models.CharField(max_length=10)
+    operator = models.CharField(max_length=10)
+    is_deleted = models.BooleanField(default=False)
+
+
+
+class Items_Total(models.Model):
+    item = models.ForeignKey(Item,on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField()
+    is_deleted = models.BooleanField(default=False)
+
