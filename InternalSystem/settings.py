178431800e34003f1da542c534be987a03e5ca81
fatinmidage <fg1048596@live.com>
2018-10-25 10:20:15 +0800

    beta 0.8.2 新增数据库

--- InternalSystem/settings.py
+++ InternalSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-^eg7)vn!b0j*4eb(_)sm(dto0@m@+fzu240oepzz5=y+zkh(y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,'static_collected')
 
 STATIC_URL = '/static/'
 
--- inport_items.py
+++ inport_items.py
@@ -6,6 +6,10 @@ from Store.models import Item
 data_frame = pd.DataFrame()
 data_frame = pd.read_excel('汇总.xlsx',
                    sheet_name='库存总表', index_col='序号')
-it_item = Item()
-print(it_item)
-print(data_frame['物品'][25])
+for i in range(len(data_frame)):
+    it = Item()
+    it.name = data_frame['物品'][i]
+    it.item_model = data_frame['规格型号'][i]
+    it.unit = data_frame['单位'][i]
+    it.location = data_frame['位置'][i]
+    it.save()
